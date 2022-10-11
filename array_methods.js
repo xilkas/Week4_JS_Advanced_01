325223b813b9cb4e37cef5622595afc2b33e279d
Abdiwali Mohamed <duranabdi@gmail.com>
2022-10-11 14:59:49 -0500

    small changes

--- array_methods.js
+++ array_methods.js
@@ -71,12 +71,3 @@ console.log(largeShirts);
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal variable and log the result
 
 console.log(ticketPriceTotal);
-
-// ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
-
-// Problem 1
-
-// Problem 2
-
-// Problem 3
--- scope_closure.js
+++ scope_closure.js
@@ -1,4 +1,3 @@
-///// M V P ///////
 /******************************************************************************\
 	Task 1: `counterMaker`
 \******************************************************************************/
@@ -78,53 +77,11 @@ function counterMaker() {
   // isNotAFriend(secondLevelFriends[2]); // true
   
   
-  /******************************************************************************\
-      Task 4: Keep a log
-  \******************************************************************************/
-  
-  /****** INSTRUCTIONS PROBLEM 4 ******/
-  /* Here we have a for loop that will iterate as long as i is less than or equal
-  to 5. What we need to do is console.log(i) so that it logs like so:
-   0 second after call - log 0
-   1 seconds after call - log 1
-   2 seconds after call - log 2
-   3 seconds after call - log 3
-   4 seconds after call - log 4
-   5 seconds after call - log 5
-   However, because each call to console.log occurs after the loop has finished,
-   the value of i has changed before the console.log executes. We'll need to use
-   a closure to preserve a reference to i at the time of execution.
-   Fix the code below to log the desired output.
-   */
-  
-  function timeOutCounter() {
-    for (var i = 0; i <= 5; i++) {
-      setTimeout(function() {
-          console.log(i)
-      }, i * 1000)
-    }
-  }
-  timeOutCounter();
-  
-  
-  /******************************************************************************\
-      Task 5: Check if name exists
-  \******************************************************************************/
-  
-  /****** INSTRUCTIONS PROBLEM 5 ******/
-  /*
-    Write a function called contains that takes in three parameters: an array, a name and a callback.  
-    Check if the name exists in the array. 
-    If it does, invoke the callback with true as the argument. 
-    If the name does not exist, invoke the callback with false as the argument.
-  */
-  
-  
   
   /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
   
   
-  // ==== Challenge 2: Create a counter function ====
+  // ==== Create a counter function ====
   const counter = () => {
       // Return a function that when invoked increments and returns a counter variable.
     };
